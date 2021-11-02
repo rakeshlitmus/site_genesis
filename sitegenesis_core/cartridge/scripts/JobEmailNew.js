a7b182a14ebcb604f07de54e1c82e57d09d00aa1
rakeshlitmus <91533862+rakeshlitmus@users.noreply.github.com>
2021-11-02 10:40:11 +0530

    Removing custom object by writting job for that.

--- sitegenesis_core/cartridge/scripts/JobEmailNew.js
+++ sitegenesis_core/cartridge/scripts/JobEmailNew.js
@@ -7,9 +7,10 @@ var file = require('dw/io/File');
 var customObjectMgr = require('dw/object/CustomObjectMgr');
 var csvWrite = require('dw/io/CSVStreamWriter');
 var fileWrite = require('dw/io/FileWriter');
+var PromotionMgr = require('dw/campaign/PromotionMgr');
+
 function execute() {
     var fileLocation = new File(File.IMPEX + File.SEPARATOR + 'src/Subscriptions' + File.SEPARATOR + 'EmailSubscription.csv');
-    // var updateFile = 'https://zzum-002.sandbox.us01.dx.commercecloud.salesforce.com/on/demandware.servlet/webdav/Sites/Impex/src/Subscriptions/EmailSubscription.csv';
     var count = 1;
     var mailList = customObjectMgr.getAllCustomObjects('footerEmailSubscribe');
     var csvWriter = new CSVStreamWriter(new FileWriter(fileLocation));
@@ -24,6 +25,33 @@ function execute() {
     }
     csvWriter.close();
 }
+
+function promotions(){
+    var promos = PromotionMgr.getPromotions();
+    var promoIter = promos.iterator();
+    while (promoIter.hasNext()) {
+     promoItem = promoIter.next();
+    }
+}
+
+function removeCustomObject(){
+    var mailObj = customObjectMgr.getAllCustomObjects('footerEmailSubscribe');
+    while (mailObj.hasNext()) {
+        customObjectMgr.remove(mailObj.next());
+    }
+}
+
+
+
 module.exports = {
     execute: execute
 };
+
+module.exports = {
+    promotions: promotions
+};
+
+module.exports = {
+    removeCustomObject: removeCustomObject
+};
+// exports.promotions = promotions;
